56a01bfed900cca561598ad843ecea4147bf4d7f
wangm7 <wangm7@mail.uc.edu>
2020-09-14 20:07:20 -0400

    Layout Instructions
    Setup
    Header/Jumbotron
    Application Chat input
    Chat messages section
    Styling the chat messages 👩‍🎨 🎨

---
+++ resources/js/index.js
@@ -0,0 +1,4 @@
+const nameInput = document.getElementById("myName");
+const myMessage = document.getElementById("myMessage");
+const sendButton = document.getElementById("sendButton");
+const chatBox = document.getElementById("chat");
