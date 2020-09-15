f626034d9d26cb521481a555004f70a44e78a198
wangm7 <wangm7@mail.uc.edu>
2020-09-14 23:44:10 -0400

    add refbtn

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,12 +1,10 @@
 const nameInput = document.getElementById("myName");
 const myMessage = document.getElementById("myMessage");
 const sendButton = document.getElementById("sendButton");
-const getButton = document.getElementById("getButton");
+const refButton = document.getElementById("refButton");
 const chatBox = document.getElementById("chat");
 const serverURL = `https://it3049c-chat-application.herokuapp.com/messages`;
 const MILLISECONDS_IN_TEN_SECONDS = 10000;
-
-
 //URL to the Server is https://it3049c-chat-application.herokuapp.com
 //URL to the link to get the messages is https://it3049c-chat-application.herokuapp.com/messages
 
@@ -20,13 +18,17 @@ function updateMessagesInChatBox() {
 function fetchMessages() {
   //fetch messages from server
   return fetch(serverURL)
-    .then(response => response.json())
+    .then(response => response.json()) // (function (reseponse) {return response.json})
+  //.catch(error => alter('Wrong'))
+
 };
 
 function formatMessage(message, myName) {
   const time = new Date(message.timestamp);
+  //loop over the recieved messages to convert them to html elements
   const formattedTime = `${time.getHours()}:${time.getMinutes()}`;
-
+  //he function will mark the messages as (incoming/outgoing) or 
+  //(yours/anyone else’s) based on the sender name in the textbox.
   if (myName === message.sender) {
     return `
          <div class="mine messages">
@@ -50,9 +52,6 @@ function formatMessage(message, myName) {
              </div>
          `
   }
-  //loop over the recieved messages to convert them to html elements
-  //he function will mark the messages as (incoming/outgoing) or 
-  //(yours/anyone else’s) based on the sender name in the textbox.
 };
 
 function updateChatBox() {
@@ -76,6 +75,50 @@ async function updateMessages() {
   });
   chatBox.innerHTML = formattedMessages;
 };
+
+
+function sendMessages(username, text) {
+  const newMessage = {
+    sender: username,
+    text: text,
+    timestamp: new Date()
+  }
+
+  fetch(serverURL, {
+    method: `POST`,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newMessage)
+  });
+}
+
+/**
+ * 
+ function sendMessages (username, text) {
+    const newMessage = {
+        sender: username,
+        text: text,
+        timestamp: new Date()
+    }
+
+    $.post(serverURL, newMessage);
+}
+ */
+sendButton.addEventListener("click", function (sendButtonClickEvent) {
+  sendButtonClickEvent.preventDefault();
+  const sender = nameInput.value;
+  const message = myMessage.value;
+
+  sendMessages(sender, message);
+  myMessage.value = "";
+});
+
+refButton.addEventListener("click", function (refreshButtonClickEvent) {
+  //window.location.reload(true);
+});
+
+
 
 updateMessages();
 setInterval(updateMessages, MILLISECONDS_IN_TEN_SECONDS);
